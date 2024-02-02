cc5c4c96c8a038a0ea6b739c02f471ac75cf6e85
Shindu <gue.shindu@gmail.com>
2024-02-02 11:12:37 +0700

    Fix add vertical scroll

--- Quiz2/app/src/main/java/com/gue/shindu/quiz2/MainActivity.kt
+++ Quiz2/app/src/main/java/com/gue/shindu/quiz2/MainActivity.kt
@@ -5,43 +5,33 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -54,9 +44,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.gue.shindu.quiz2.ui.theme.Purple40
 import com.gue.shindu.quiz2.ui.theme.Quiz2Theme
 
 class MainActivity : ComponentActivity() {
@@ -66,8 +54,7 @@ class MainActivity : ComponentActivity() {
             Quiz2Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     MainDisplay()
                 }
@@ -77,12 +64,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainDisplay() {
-    Column (
+fun MainDisplay(modifier: Modifier = Modifier) {
+    Column(
         verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding(),
+    ) {
+        Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(Modifier.height(20.dp))
@@ -103,8 +93,7 @@ fun PartLogo(modifier: Modifier = Modifier) {
         painter = painterResource(R.drawable.logo),
         contentDescription = stringResource(R.string.logo_desc),
         contentScale = ContentScale.FillWidth,
-        modifier = modifier
-            .padding(horizontal = 120.dp, vertical = 7.dp)
+        modifier = modifier.padding(horizontal = 120.dp, vertical = 7.dp)
     )
 }
 
@@ -132,12 +121,13 @@ fun PartLogin(modifier: Modifier = Modifier) {
                 .align(alignment = Alignment.Start)
         )
         EditField(
-            value = nipState.value ,
-            onValueChanged = {nipState.value = it} ,
-            keyboardOptions =  KeyboardOptions.Default.copy(
+            value = nipState.value,
+            onValueChanged = { nipState.value = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
                 keyboardType = KeyboardType.Text,
                 capitalization = KeyboardCapitalization.Characters,
-                imeAction = ImeAction.Next),
+                imeAction = ImeAction.Next
+            ),
             modifier = Modifier.fillMaxWidth()
         )
         Text(
@@ -148,62 +138,49 @@ fun PartLogin(modifier: Modifier = Modifier) {
                 .align(alignment = Alignment.Start)
         )
         EditField(
-            value = passwordState.value ,
-            onValueChanged = { passwordState.value = it } ,
-            keyboardOptions =  KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done),
+            value = passwordState.value,
+            onValueChanged = { passwordState.value = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Password, imeAction = ImeAction.Done
+            ),
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(35.dp))
         Button(
             onClick = {
                 Toast.makeText(
-                    ctx,
-                    cekLogin(
-                        nipState.value,
-                        passwordState.value,
-                        ctx
-                    ),
-                    Toast.LENGTH_LONG
+                    ctx, cekLogin(
+                        nipState.value, passwordState.value, ctx
+                    ), Toast.LENGTH_LONG
                 ).show()
-                 },
-            shape =  RoundedCornerShape(12.dp),
-            modifier = Modifier
+            }, shape = RoundedCornerShape(12.dp), modifier = Modifier
                 .fillMaxWidth()
                 .height(52.dp)
-            ) {
+        ) {
             Text(
-                style =  MaterialTheme.typography.labelSmall,
-                text = stringResource(R.string.login)
+                style = MaterialTheme.typography.labelSmall, text = stringResource(R.string.login)
             )
 
         }
-
     }
 }
 
 private fun cekLogin(nip: String, pwd: String, context: Context): String {
-    var msg : String
-    if (nip.isEmpty() && pwd.isEmpty()) msg = context.getString(R.string.nip_pwd_failed)
-    else if (nip.isEmpty()) msg = context.getString(R.string.nip_fail)
-    else if (pwd.isEmpty()) msg = context.getString(R.string.pwd_fail)
-    else if (nip.startsWith("AR"))
-        msg = context.getString(R.string.menuju_halaman_beranda)
-    else
-        msg = context.getString(R.string.format_nip_arn_art)
-    return msg
-
+    return if (nip.isEmpty() && pwd.isEmpty()) context.getString(R.string.nip_pwd_failed)
+    else if (nip.isEmpty()) context.getString(R.string.nip_fail)
+    else if (pwd.isEmpty()) context.getString(R.string.pwd_fail)
+    else if (nip.startsWith("AR")) context.getString(R.string.menuju_halaman_beranda)
+    else context.getString(R.string.format_nip_arn_art)
 }
 
 @Composable
-fun PartHelp () {
+fun PartHelp() {
     val ctx = LocalContext.current
 
-    var hubCSIkhwan = stringResource(R.string.menghubungi_cs_ikhwan)
-    var hubCSAkhwat = stringResource(R.string.menghubungi_cs_akhwat)
+    val hubCSIkhwan = stringResource(R.string.menghubungi_cs_ikhwan)
+    val hubCSAkhwat = stringResource(R.string.menghubungi_cs_akhwat)
 
-    Column (
+    Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
@@ -212,29 +189,20 @@ fun PartHelp () {
         )
         Spacer(modifier = Modifier.height(20.dp))
         ClickableText(
-            text = AnnotatedString(stringResource(R.string.hubungi_cs_ikhwan)),
-            onClick = {
+            text = AnnotatedString(stringResource(R.string.hubungi_cs_ikhwan)), onClick = {
                 Toast.makeText(
-                    ctx,
-                    hubCSIkhwan,
-                    Toast.LENGTH_LONG
+                    ctx, hubCSIkhwan, Toast.LENGTH_LONG
                 ).show()
-            },
-            style = MaterialTheme.typography.labelSmall.copy(color = Color.Blue)
+            }, style = MaterialTheme.typography.labelSmall.copy(color = Color.Blue)
         )
         Spacer(modifier = Modifier.height(20.dp))
         ClickableText(
-            text = AnnotatedString(stringResource(R.string.hubungi_cs_akhwat)),
-            onClick = {
+            text = AnnotatedString(stringResource(R.string.hubungi_cs_akhwat)), onClick = {
                 Toast.makeText(
-                    ctx,
-                    hubCSAkhwat,
-                    Toast.LENGTH_LONG
+                    ctx, hubCSAkhwat, Toast.LENGTH_LONG
                 ).show()
-            },
-            style = MaterialTheme.typography.labelSmall.copy(color = Color.Blue)
+            }, style = MaterialTheme.typography.labelSmall.copy(color = Color.Blue)
         )
-
     }
 }
 
@@ -243,36 +211,31 @@ fun EditField(
     value: String,
     onValueChanged: (String) -> Unit,
     keyboardOptions: KeyboardOptions,
+    modifier: Modifier = Modifier,
     label: String = "",
-    modifier: Modifier = Modifier
 ) {
     TextField(
         value = value,
         singleLine = true,
         onValueChange = onValueChanged,
-        label ={ Text( label ) },
+        label = { Text(label) },
         keyboardOptions = keyboardOptions,
-        visualTransformation = if (keyboardOptions.keyboardType == KeyboardType.Password)
-            PasswordVisualTransformation() else
-            VisualTransformation.None,
-        modifier = modifier
-            .clip(RoundedCornerShape(12.dp)),
+        visualTransformation = if (keyboardOptions.keyboardType == KeyboardType.Password) PasswordVisualTransformation() else VisualTransformation.None,
+        modifier = modifier.clip(RoundedCornerShape(12.dp)),
     )
 }
 
 @Composable
 fun FAQField() {
     val ctx = LocalContext.current
-    var faq = stringResource(R.string.ke_halaman_faq)
+    val faq = stringResource(R.string.ke_halaman_faq)
     Button(
         onClick = {
             Toast.makeText(
-                ctx,
-                faq,
-                Toast.LENGTH_LONG
+                ctx, faq, Toast.LENGTH_LONG
             ).show()
         },
-        shape =  RoundedCornerShape(12.dp),
+        shape = RoundedCornerShape(12.dp),
         colors = ButtonDefaults.buttonColors(containerColor = Color.White),
         border = BorderStroke(1.dp, Color.Blue),
         modifier = Modifier
@@ -282,17 +245,19 @@ fun FAQField() {
             .padding(horizontal = 16.dp)
     ) {
         Text(
-            style =  MaterialTheme.typography.labelSmall.copy(color= Color.Blue),
+            style = MaterialTheme.typography.labelSmall.copy(color = Color.Blue),
             text = stringResource(R.string.lihat_faq)
-
         )
-
     }
 }
 
 @Composable
 fun BottomPart() {
-     FAQField()
+    Column {
+        Spacer(modifier = Modifier.height(16.dp))
+        FAQField()
+        Spacer(modifier = Modifier.height(16.dp))
+    }
 }
 
 @Preview(showBackground = true)
